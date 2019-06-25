a7287dd6129630c42bda5a4fa1cbad824588c2ad
Psyfo <21422869@github.com>
2019-06-25 22:30:29 +0200

    Navigation for Services

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,3 +1,32 @@
-$(document).ready(function() {
-    
+$(document).ready(function() {});
+// Service dropdown
+$(function() {
+    'use strict';
+
+    function uncheckBox() {
+        var isChecked = $('#services-menu').prop('checked');
+        if (isChecked) {
+            $('#services-menu').prop('checked', false);
+        }
+    }
+    function checkBox() {
+        var isChecked = $('#services__toggler').prop('checked');
+        if (!isChecked) {
+            $('#services__toggler').prop('checked', true);
+        }
+        if (isChecked) {
+            $('#services__toggler').prop('checked', false);
+        }
+    }
+
+    $('body').on('click', function() {
+        uncheckBox();
+    });
+    $('.navigation__icon').on('click', function() {
+        checkBox();
+    });
+
+    $('#services-menu,label').on('click', function(e) {
+        e.stopPropagation();
+    });
 });
