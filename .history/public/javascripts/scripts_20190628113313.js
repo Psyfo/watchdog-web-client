cbbfda16094adcb9bb6f5b6a881379b1ef34452c
Psyfo <21422869@dut4life.ac.za>
2020-07-28 23:28:58 +0200

    re-added social media link

---
+++ .history/public/javascripts/scripts_20190628113313.js
@@ -0,0 +1,44 @@
+import anime from 'animejs';
+
+$(document).ready(function() {});
+// Service dropdown
+$(function() {
+    'use strict';
+
+    function uncheckBox() {
+        var isChecked = $('#services-menu').prop('checked');
+        if (isChecked) {
+            $('#services-menu').prop('checked', false);
+        }
+    }
+    function checkBox() {
+        var isChecked = $('#services__toggler').prop('checked');
+        if (!isChecked) {
+            $('#services__toggler').prop('checked', true);
+        }
+        if (isChecked) {
+            $('#services__toggler').prop('checked', false);
+        }
+    }
+
+    $('body').on('click', function() {
+        uncheckBox();
+    });
+    $('.navigation__icon').on('click', function() {
+        checkBox();
+    });
+
+    $('#services-menu,label').on('click', function(e) {
+        e.stopPropagation();
+    });
+});
+
+$(document).ready(function() {
+    var servicesMenu = () => {
+        anime({
+            targets: '.services__menu',
+            duration: 300,
+        });
+    };
+    servicesMenu();
+});
---
+++ .history/routes/index_20190625165923.js
@@ -0,0 +1,69 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Home' });
+});
+
+router.get('/about', (req, res, next) => {
+    res.render('about', {
+        title: 'About',
+    });
+});
+
+router.get('/services', (req, res, next) => {
+    res.render('services', {
+        title: 'Services',
+    });
+});
+
+router.get('/services-video', (req, res, next) => {
+    res.render('services-video', {
+        title: 'Video Production Services',
+    });
+});
+
+router.get('/services-event', (req, res, next) => {
+    res.render('services-event', {
+        title: 'Event Videography',
+    });
+});
+
+router.get('/services-corporate', (req, res, next) => {
+    res.render('services-corporate', {
+        title: 'Corporate/Training Videos',
+    });
+});
+
+router.get('/services-social', (req, res, next) => {
+    res.render('services-social', {
+        title: 'Social Media',
+    });
+});
+
+router.get('/people', (req, res, next) => {
+    res.render('people', {
+        title: 'People',
+    });
+});
+
+router.get('/palesa', (req, res, next) => {
+    res.render('palesa', {
+        title: 'Palesa',
+    });
+});
+
+router.get('/contact-us', (req, res, next) => {
+    res.render('contact-us', {
+        title: 'Contact Us',
+    });
+});
+
+router.get('/previous-work', (req, res, next) => {
+    res.render('previous-work', {
+        title: 'Previous Work',
+    });
+});
+
+module.exports = router;
